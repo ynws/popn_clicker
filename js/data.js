202cdf5ef5f33915b26e1740a4d9671aed2c32fc
wanau <>
2026-07-19 06:14:41 +0900

    add achievement

--- js/data.js
+++ js/data.js
@@ -42,13 +42,13 @@ const UPGRADE_DEFS = [
   { id:"g4_10", name:"あまいおくすり", iconSrc: "icon/candy.png", desc: "真っ暗な夜にとけこんで、ちぇるみんの能力が2倍になるよ", cost:150000, type:"targetMult", target:"g4", value:2, unlock:[ {type:"generatorLevel",target:"g4",value:10} ] },
   { id:"g4_20", name:"まっぷたツートンソウル", iconSrc: "icon/soul.png", desc: "運命の相手が見つかって、ちぇるみんの能力が5倍になるよ", cost:900000, type:"targetMult", target:"g4", value:5, unlock:[ {type:"generatorLevel",target:"g4",value:20} ] },
 
-  { id:"g5_10", name:"テクニカルマイコンパクト", iconSrc: "icon/compact.png", desc: "変身アイテムでスペース🪐マコの能力が3倍になるよ", cost:1500000, type:"targetMult", target:"g5", value:3, unlock:[ {type:"generatorLevel",target:"g5",value:10} ] },
+  { id:"g5_10", name:"テクニカル・マジカル・マイ・コンパクト", iconSrc: "icon/compact.png", desc: "変身アイテムでスペース🪐マコの能力が3倍になるよ", cost:1500000, type:"targetMult", target:"g5", value:3, unlock:[ {type:"generatorLevel",target:"g5",value:10} ] },
   { id:"g5_20", name:"キング様からの応援", iconSrc: "icon/king.png", desc: "憧れの人からの応援でスペース🪐マコの能力が5倍になるよ", cost:9000000, type:"targetMult", target:"g5", value:5, unlock:[ {type:"generatorLevel",target:"g5",value:20} ] },
 
   { id:"g6_10", name:"コスプレ衣装", iconSrc: "icon/scissors.png", desc: "こっそり着替えてニアちゃんの能力が3倍になるよ", cost:15000000, type:"targetMult", target:"g6", value:3, unlock:[ {type:"generatorLevel",target:"g6",value:10} ] },
   { id:"g6_20", name:"暗くて狭い部屋", iconSrc: "icon/room.png", desc: "落ち着く環境でニアちゃんの能力が6倍になるよ", cost:90000000, type:"targetMult", target:"g6", value:6, unlock:[ {type:"generatorLevel",target:"g6",value:20} ] },
 
-  { id:"g8_7", name:"ネクタル", iconSrc: "icon/nectar.png", desc: "神々のためのお酒。シグマ様の能力が3倍になるよ", cost:150000001, type:"targetMult", target:"g7", value:3, unlock:[ {type:"generatorLevel",target:"g7",value:7} ] },
+  { id:"g7_7", name:"ネクタル", iconSrc: "icon/nectar.png", desc: "神々のためのお酒。シグマ様の能力が3倍になるよ", cost:150000001, type:"targetMult", target:"g7", value:3, unlock:[ {type:"generatorLevel",target:"g7",value:7} ] },
   { id:"g7_17", name:"シグマ様親衛隊", iconSrc: "icon/lithos.png", desc: "リソスとその仲間の力で、シグマ様の能力が7倍になるよ", cost:900000011, type:"targetMult", target:"g7", value:7, unlock:[ {type:"generatorLevel",target:"g7",value:17} ] },
 ];
 
@@ -61,8 +61,15 @@ const ACHIEVEMENT_DEFS = [
   { id: "earn_6573", name: "トイサイダー村", iconSrc: "icon/minipop.png", desc: "累計で6573ノーツを獲得する", unlock: [{ type: "totalNotesCount", value: 6573 }] },
   { id: "earn_50all", name: "Lv50全ノーツ相当", iconSrc: "icon/minipop.png", desc: "(THX 4まで全26譜面)累計で46115ノーツを獲得する", unlock: [{ type: "totalNotesCount", value: 46115 }] },
   { id: "first_upgrade", name: "アップグレード！", iconSrc: "icon/nyami.png", desc: "はじめて強化をする", unlock: [{ type: "upgradesOwnedCount", value: 1 }] },
+  { id: "upgrade10", name: "たくさんのアップグレード", iconSrc: "icon/minipop.png", desc: "強化を10個購入する", unlock: [{ type: "upgradesOwnedCount", value: 10 }] },
   { id: "playtime_16min", name: "タイムプレーモード", iconSrc: "icon/minipop.png", desc: "累計16分間プレイする", unlock: [{ type: "playTimeSeconds", value: 960 }] },
   { id: "first_generator", name: "キャラクターご招待", iconSrc: "icon/mimi.png", desc: "はじめてキャラクターを呼ぶ", unlock: [{ type: "generatorLevel", target: "g1", value: 1 }] },
+  { id: "mimi_chan", name: "３３ちゃん", iconSrc: "icon/mimi.png", desc: "ミミちゃんのレベルを33にする", unlock: [{ type: "generatorLevel", target: "g1", value: 33 }] },
+  { id: "nyami_chan", name: "２８ちゃん", iconSrc: "icon/nyami.png", desc: "ニャミちゃんのレベルを28にする", unlock: [{ type: "generatorLevel", target: "g2", value: 28 }] },
+  { id: "angel_poet", name: "立派な天使", iconSrc: "icon/poet.png", desc: "ポエットちゃんのレベルを20にする", unlock: [{ type: "generatorLevel", target: "g3", value: 20 }] },
+  { id: "angel_chermin", name: "出身地：天使突抜", iconSrc: "icon/chermin.png", desc: "ちぇるみんのレベルを14にする", unlock: [{ type: "generatorLevel", target: "g4", value: 14 }] },
+  { id: "mako_attack", name: "スペース★アタック", iconSrc: "icon/mako.png", desc: "マコちゃんのレベルを15にする", unlock: [{ type: "generatorLevel", target: "g5", value: 15 }] },
+  { id: "idol_nia", name: "electro idol nia", iconSrc: "icon/nia.png", desc: "ニアちゃんのレベルを15にする", unlock: [{ type: "generatorLevel", target: "g6", value: 15 }] },
   { id: "first_sigma", name: "素数の世界へ", iconSrc: "icon/sigma.png", desc: "はじめてΣ様を呼ぶ", unlock: [{ type: "generatorLevel", target: "g7", value: 1 }] },
   { id: "end", name: "おしまい", iconSrc: "icon/sigma.png", desc: "今このゲームでできることは大体全部終わったよ。また遊んでね", unlock: [{ type: "generatorLevel", target: "g7", value: 17 }] },
 ];
